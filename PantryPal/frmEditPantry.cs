2961d13163783cde485145d2fbbceb204c654025
Jon Barnette <jonathan.barnette@gmail.com>
2018-12-05 10:18:33 -0500

    testing

--- PantryPal/frmEditPantry.cs
+++ PantryPal/frmEditPantry.cs
@@ -24,31 +24,33 @@ namespace PantryPal
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            if (txtPanItem1.Text != "")
+            if (txtPanItem1.Text != "" && !Program.pantryItems.Contains(txtPanItem1.Text))
                 Program.pantryItems.Add(txtPanItem1.Text);
-            if (txtPanItem2.Text != "")
+            if (txtPanItem2.Text != "" && !Program.pantryItems.Contains(txtPanItem2.Text))
                 Program.pantryItems.Add(txtPanItem2.Text);
-            if (txtPanItem3.Text != "")
+            if (txtPanItem3.Text != "" && !Program.pantryItems.Contains(txtPanItem3.Text))
                 Program.pantryItems.Add(txtPanItem3.Text);
-            if (txtPanItem4.Text != "")
+            if (txtPanItem4.Text != "" && !Program.pantryItems.Contains(txtPanItem4.Text))
                 Program.pantryItems.Add(txtPanItem4.Text);
-            if (txtPanItem5.Text != "")
+            if (txtPanItem5.Text != "" && !Program.pantryItems.Contains(txtPanItem5.Text))
                 Program.pantryItems.Add(txtPanItem5.Text);
-            if (txtPanItem6.Text != "")
+            if (txtPanItem6.Text != "" && !Program.pantryItems.Contains(txtPanItem6.Text))
                 Program.pantryItems.Add(txtPanItem6.Text);
-            if (txtPanItem7.Text != "")
+            if (txtPanItem7.Text != "" && !Program.pantryItems.Contains(txtPanItem7.Text))
                 Program.pantryItems.Add(txtPanItem7.Text);
-            if (txtPanItem8.Text != "")
+            if (txtPanItem8.Text != "" && !Program.pantryItems.Contains(txtPanItem8.Text))
                 Program.pantryItems.Add(txtPanItem8.Text);
-            if (txtPanItem9.Text != "")
+            if (txtPanItem9.Text != "" && !Program.pantryItems.Contains(txtPanItem9.Text))
                 Program.pantryItems.Add(txtPanItem9.Text);
+
+            tblPantryItems.Items.Clear();
+
+            
 
             for (int i = 0; i < Program.pantryItems.Count; i++)
             {
                tblPantryItems.Items.Add(Program.pantryItems[i].ToString());
             }
-
-            tblPantryItems.Refresh();
 
             txtPanItem1.Clear();
             txtPanItem2.Clear();
